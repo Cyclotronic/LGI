1faab44a84e8a5273c0bf80a6044cc61c60b06a4
Cyclotronic <crimp@hotmail.com>
2026-08-05 21:39:37 -0400

    Update macos runner image and release v1.0.5

--- lgi_core.py
+++ lgi_core.py
@@ -44,7 +44,7 @@ from typing import Callable, Iterable, Optional
 
 APP_NAME = "LGI"
 APP_TITLE = "LAN GPIB Inventory"
-VERSION = "1.0.4"
+VERSION = "1.0.5"
 
 DEFAULT_DB_PATH = Path.home() / ".lgi" / "inventory.sqlite3"
 
